0fbffedf2c4a04b87dbc5d4fc6dae26e93aeef2b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 12:06:35 +0100

    fix: refactor

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -5,11 +5,7 @@ const ProtectedRoute = ({ children }) => {
   const location = useLocation();
   const { user } = useSelector((state) => state.user);
 
-  if (user) {
-    return <Navigate to='/' replace state={{ from: location }} />;
-  }
-
-  return children;
+  return user ? <Navigate to='/' replace state={{ from: location }} /> : children;
 };
 
 export default ProtectedRoute;
