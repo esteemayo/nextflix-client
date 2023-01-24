fb9e96ee27ed25207b2f0678cc0f04fde0fb30bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 11:57:28 +0100

    fix: refactor

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,10 +3,8 @@ import http from './httpService';
 const apiEndpoint = '/auth';
 const tokenKey = 'accessToken';
 
-export function login(credentials) {
-  return http.post(`${apiEndpoint}/login`, credentials);
-}
+export const login = (credentials) =>
+  http.post(`${apiEndpoint}/login`, credentials);
 
-export function getJWT() {
-  return localStorage.getItem(tokenKey);
-}
+export const getJWT = () => localStorage.getItem(tokenKey);
+
