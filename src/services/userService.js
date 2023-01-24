093f5892e04b33bff6e45ac459212d7529966d3a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 11:53:46 +0100

    first commit

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import http from './httpService';
 
-const apiEndpoint = 'users';
+const apiEndpoint = '/users';
 
 export function register(credentials) {
   return http.post(`${apiEndpoint}/register`, credentials);
