1f2b75a46ca7b4c4ff991243bd0cfd0ff0c4602d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 12:01:15 +0100

    fix: modify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
-import Error from './Error/Error';
+import NotFound from './notFound/NotFound';
 import Home from './home/Home';
 import Login from './login/Login';
 import Register from './register/Register';
 import SharedLayout from './sharedLayout/SharedLayout';
 import Watch from './watch/Watch';
 
-export { Error, Home, Login, Register, SharedLayout, Watch };
+export {
+  Home,
+  Login,
+  NotFound,
+  Register,
+  SharedLayout,
+  Watch,
+};
